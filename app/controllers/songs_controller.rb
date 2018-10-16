00703706487bb06b2d13559caf2bb0d53360cce1
Brian DiRito <bdirito@gmail.com>
2018-10-16 11:48:15 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,15 +1,18 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
+    @song = Song.new
   end
 
   def create
-    @song = Song.new(song_params)
+    @song = Song.new(title: song_params[:title], artist_id: Artist.find_by(name: song_params[:artist]).id)
 
     if @song.save
       redirect_to @song
@@ -44,7 +47,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist)
   end
 end
-
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,8 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+
+  def song_count
+    songs = Song.all.select {|song| song.artist_id == self.id}
+    count = songs.count
+  end
 end
